0c0bb2d62e06ca8518c3bbf1f063195d7d0568ec
buzzi <buzzi.javier@gmail.com>
2017-07-31 10:30:57 +0200

    Adds comments to python source

--- annoy_on_twitter.py
+++ annoy_on_twitter.py
@@ -9,8 +9,10 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
+# calculate a number that im willing to live with -- 40% under the speed i pay for
 ALLOWED_THRESHOLD = .40
 
+# speed i pay for in bits
 PAY_FOR = {
   'download': 300000000,
   'upload': 300000000
@@ -24,6 +26,12 @@ la data esta aqui: https://github.com/kingbuzzman/vodafone_annoy
 
 
 def filerev(somefile, buffer=0x20000):
+    """
+    Reads the file backwards (think tail)
+
+    Thanks to Ignacio Vazquez-Abrams (https://stackoverflow.com/questions/2301789/read-a-file-in-reverse-order-using-python#answer-2301867)  # noqa
+    I don't love it, i can see this working without the 're' -- but i didnt want to spend the time to fix it
+    """
     somefile.seek(0, os.SEEK_END)
     size = somefile.tell()
     lines = ['']
@@ -44,6 +52,9 @@ def filerev(somefile, buffer=0x20000):
 
 
 def annoy_vodafone(download, upload):
+    """
+    Send the tweet
+    """
     api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 
     with open('graph.png', 'rb') as image:
@@ -55,17 +66,23 @@ def annoy_vodafone(download, upload):
 
 
 def main():
+    # open the csv file
     with open('vodafone.csv', 'rb') as csvfile:
+        # read the very last line
         last_line = [next(filerev(csvfile))]
+        # parse the very last line
         server_id, server_sponsor, server_name, timestamp, \
             server_d, ping, download, upload, wifi_name, router_ping = next(csv.reader(last_line, delimiter=','))
 
+        # convert the values from str to float
         download = float(download)
         upload = float(upload)
 
+        # calculate the minimun speed im willing to tolerate without bugging them about it (40% of my current speed)
         threshold_download = PAY_FOR['download'] - (PAY_FOR['download'] * ALLOWED_THRESHOLD)
         threshold_upload = PAY_FOR['upload'] - (PAY_FOR['upload'] * ALLOWED_THRESHOLD)
 
+        # if what im willing to have is lower than what i have, annoy them!
         if threshold_download > download or threshold_upload > upload:
             annoy_vodafone(download / (1000**2), upload / (1000**2))
 
